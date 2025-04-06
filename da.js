c83f7f734a66ce85849627f842a7db030c644da8
grishapir <114808385+grishapir@users.noreply.github.com>
2025-04-06 18:59:40 +0700

    Add files via upload

--- da.js
+++ da.js
@@ -1,5 +1,106 @@
-let firstName, dlina;
-firstName=prompt("da");
-dlina=firstName.length - 1;
-console.log(firstName.slice(dlina,1000000000000));
-alert(firstName.slice(dlina,100000000000000000));
+/*let a=prompt("ВВеди")
+a=Number(a)
+if (100=== a){
+    alert(100+=a)
+}
+else{
+    a=prompt("ВВеди")
+}
+*/
+/*let b=prompt("Введи")
+b=Number(b)
+if(b === 20)
+    {
+        alert(b*b)
+    }
+else{
+    alert(b)
+}
+    */
+/*let age=prompt("Тебе 20?")
+Number(age)
+if(age<= 17){
+    alert("Ай ай ай обманщик")
+}
+else{
+    alert("Я слежу за тобой")
+}
+*/
+/*let chislo=prompt("Введи")
+Number(chislo)
+if(chislo>=0){
+    alert("Ну ладно")
+}
+else{
+    alert("Неладно")
+}
+*/
+/*let k,o;
+k=prompt("Первое слово")
+o=prompt("Первое слово")
+if(k.charAt(0)=== o.charAt(0)){
+    alert("da")
+}
+else{
+    alert(o)
+}
+
+let a,b;
+a=prompt("Число")
+b=prompt("Число")
+Number(a,b)
+if(a%b==0){
+    alert("da")
+}
+else{
+    alert("net")
+}
+
+let id=prompt("Введи ид товара")
+switch(id){
+    case '1':
+        alert("10")
+    case '2':
+        alert("256")
+    case '3':
+        alert("53")
+    case '4':
+        alert("3")
+    default:
+        alert("net")
+    break
+}
+
+//там нет{}
+let a,b;
+a=1
+b=a*5<6 ? true : false
+alert(b)
+
+let a,b,c,k;
+k=prompt("пароль")
+Number(k)
+a=95831747
+b=33317922
+c=94558997
+
+if(k== a){
+        alert("A,B,C")
+}
+else if(k== b){
+        alert("B,C")
+}
+else if(k== c){
+        alert("C")
+}
+
+let a=1,b=2,max=0;
+if(a>b){
+    max=a
+    alert(max)
+}
+else{
+    max = b
+    alert(max)
+}
+*/
